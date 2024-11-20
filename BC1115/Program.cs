c3f7baf3d3d11cf157881f9b7486665d70bdb414
niconishiyama <nicolasnishiyama@gmail.com>
2024-11-20 17:52:49 -0300

    BeeCrowd 1115

---
+++ BC1115/Program.cs
@@ -0,0 +1,29 @@
+﻿int x, y;
+
+string[] values = Console.ReadLine().Split(' ');
+
+x = int.Parse(values[0]);
+y = int.Parse(values[1]);
+
+while (x != 0 & y != 0)
+{
+    if (x > 0 & y > 0)
+    {
+        Console.WriteLine("primeiro");
+    }
+    else if (x < 0 & y > 0)
+    {
+        Console.WriteLine("segundo");
+    }
+    else if (x < 0 & y < 0)
+    {
+        Console.WriteLine("terceiro");
+    }
+    else
+    {
+        Console.WriteLine("quarto");
+    }
+    values = Console.ReadLine().Split(' ');
+    x = int.Parse(values[0]);
+    y = int.Parse(values[1]);
+}
