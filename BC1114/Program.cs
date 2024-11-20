e0dd8357913dd020e0cefb68bcacd1745077b890
niconishiyama <nicolasnishiyama@gmail.com>
2024-11-20 17:13:18 -0300

    BeeCrowd 1114

---
+++ BC1114/Program.cs
@@ -0,0 +1,12 @@
+﻿int senha = int.Parse(Console.ReadLine());
+
+while (senha != 2002)
+{
+    Console.WriteLine("Senha Invalida");
+    senha = int.Parse(Console.ReadLine());
+        if (senha == 2002)
+    {
+        Console.WriteLine("Acesso Permitido");
+    }
+}
+
