402fe9ee4cf5c63108d41493379b10297616674b
niconishiyama <nicolasnishiyama@gmail.com>
2024-11-21 20:56:51 -0300

    BeeCrowd1037

---
+++ BC1037/Program.cs
@@ -0,0 +1,33 @@
+﻿Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+double valor;
+string intervalo = " ";
+
+valor = double.Parse(Console.ReadLine());
+
+if (valor >= 0 & valor <= 25.00)
+{
+    intervalo = "[0,25]";
+    Console.WriteLine("Intervalo {0}", intervalo);
+}
+else if (valor > 25 & valor <= 50)
+{
+    intervalo = "(25,50]";
+    Console.WriteLine("Intervalo {0}", intervalo);
+}
+else if (valor > 50 & valor <= 75)
+{
+    intervalo = "(50,75]";
+    Console.WriteLine("Intervalo {0}", intervalo);
+}
+else if (valor > 50 & valor <= 100)
+{
+    intervalo = "(75,100]";
+    Console.WriteLine("Intervalo {0}", intervalo);
+}
+else
+{
+    Console.WriteLine("Fora de intervalo");
+}
+
+Console.ReadLine();
