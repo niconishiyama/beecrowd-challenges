830d9ed47498cb7442be10fc965d9964db9da036
niconishiyama <nicolasnishiyama@gmail.com>
2024-11-25 20:27:21 -0300

    BeeCrowd1067

---
+++ BC1067/Program.cs
@@ -0,0 +1,13 @@
+﻿int X, i, impares;
+
+X = int.Parse(Console.ReadLine());
+impares = 0;
+
+for (i = 1; i <= X; i++)
+{
+   if (i % 2 != 0)
+    {
+        impares = i;
+        Console.WriteLine(impares);
+    }
+}
