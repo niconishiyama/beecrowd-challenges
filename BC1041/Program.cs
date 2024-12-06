25190555886dd9bfd44926c988224ae600ccb993
niconishiyama <nicolasnishiyama@gmail.com>
2024-12-06 20:05:40 -0300

    BeeCrowd1041

---
+++ BC1041/Program.cs
@@ -0,0 +1,37 @@
+﻿Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+double X, Y;
+
+string[] values = Console.ReadLine().Split(' ');
+
+X = double.Parse(values[0]);
+Y = double.Parse(values[1]);
+
+if (X > 0.0 && Y > 0.0)
+{
+    Console.WriteLine("Q1");
+}
+else if (X < 0.0 && Y > 0.0)
+{
+    Console.WriteLine("Q2");
+}
+else if (X < 0.0 && Y < 0.0)
+{
+    Console.WriteLine("Q3");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Q4");
+}
+else if (X == 0 & Y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if (X < 0 || X > 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if (Y < 0 || Y > 0)
+{
+    Console.WriteLine("Eixo Y");
+}
